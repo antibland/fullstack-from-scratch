cd62fddb076e81b9fdaed7afe0152b744982ccb2
Andy Hoffman <antibland@gmail.com>
2022-08-02 14:34:16 -0700

    Initial gradient styles for homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,22 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
+import GradientLayout from "../components/gradientLayout";
 import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
-  return <div>home</div>;
+  return (
+    <GradientLayout
+      color="red"
+      subtitle="Profile"
+      title="Andy Hoffman"
+      description="15 public playlists"
+      image="https://tinted-gym-f99.notion.site/image/https%3A%2F%2Fdl.dropboxusercontent.com%2Fs%2Fbgiv0ssz3xpotz9%2Fpeep.png%3Fdl%3D0?table=block&id=33f9771b-0e6f-4a72-832c-69ed2d41f290&spaceId=511cd811-5561-4a61-b550-c4086b4afafb&width=2000&userId=&cache=v2"
+      roundImage
+    >
+      <div>Home page</div>
+    </GradientLayout>
+  );
 };
 
 export default Home;
