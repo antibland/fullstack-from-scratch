81f6f6ebf9ada8ef91f3a03b13be6ed82124fd7d
Andy Hoffman <antibland@gmail.com>
2022-08-04 22:17:53 -0700

    Ignore ts build errors

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  typescript: {
+    ignoreDevErrors: true,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
